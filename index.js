54b7524e36fa21d7ab7ff988f9e330d508d3ef40
AbhinavAjith2 <119229899+AbhinavAjith2@users.noreply.github.com>
2022-11-27 21:24:17 +0400

    Add files via upload

---
+++ index.js
@@ -0,0 +1,15 @@
+$(".button2min").click(function () {
+  setInterval(function () { alert("drink water!") }, 2 * 60 * 1000);
+});
+$(".button30min").click(function () {
+  setInterval(function () { alert("drink water!") }, 30 * 60 * 1000);
+});
+$(".button1hour").click(function () {
+  setInterval(function () { alert("drink water!") }, 60 * 60 * 1000);
+});
+$(".button2hour").click(function () {
+  setInterval(function () { alert("drink water!") }, 120 * 60 * 1000);
+});
+$(".button3hour").click(function () {
+  setInterval(function () { alert("drink water!") }, 180 * 60 * 1000);
+});
